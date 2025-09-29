10426671f203667f718e6e5f9f02861a93f6fb9f
jonathan-blomlof <joning.b@hotmail.com>
2025-09-29 13:57:02 +0200

    Add fingerprint for browser-permissions

--- fingerprinting.js
+++ fingerprinting.js
@@ -13,6 +13,7 @@ async function generateFingerprint() {
     const fingerprint = {
         agent: navigator.userAgent,
         // geolocation: location,
+        permissions: await getPermissions(),
         languages: navigator.languages
     }
     return fingerprint;
@@ -25,4 +26,49 @@ async function hashFingerprint(fingerprint) {
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
     return hashHex;
+}
+
+async function getPermissions() {
+    const permissions = [
+        "accelerometer",
+        "accessibility-events",
+        "ambient-light-sensor",
+        "background-sync",
+        "camera",
+        "clipboard-read",
+        "clipboard-write",
+        "geolocation",
+        "gyroscope",
+        "local-fonts",
+        "magnetometer",
+        "microphone",
+        "midi",
+        "notifications",
+        "payment-handler",
+        "persistent-storage",
+        "push",
+        "screen-wake-lock",
+        "storage-access",
+        "top-level-storage-access",
+        "window-management",
+    ];
+    let permissionStatus = [];
+    for (const permission of permissions) {
+        try {
+            let res;
+            if (permission === "top-level-storage-access") {
+                res = await navigator.permissions.query({
+                    name: permission,
+                    requestedOrigin: window.location.origin,
+                });
+            } else {
+                res = await navigator.permissions.query({ name: permission });
+            }
+            permissionStatus.push(res.state);
+        }
+        catch (error) {
+            permissionStatus.push('Not supported');
+        }
+    }
+    return permissionStatus;
 }
