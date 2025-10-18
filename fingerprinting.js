2a26fe01a7d857fa5ecfaba32092d3f341980f49
sumeyayasir <124312355+sumeyayasir@users.noreply.github.com>
2025-10-18 22:40:10 +0200

    cookie test+åtgärder

--- fingerprinting.js
+++ fingerprinting.js
@@ -28,7 +28,14 @@ async function hashFingerprint(fingerprint) {
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
     return hashHex;
 }
-
+//function TestCookies()
+{
+    //SameSite
+    document.cookie = "username=sumeya; path=/; SameSite=Strict; max-age=3600";
+    document.cookie = "sessionID=789129aa; path=/; SameSite=Strict; max-age=3600";
+    alert("Cookies have been set for testing.");
+    location.reload();
+}
 //Cookies funktion
 async function getCookieFingerprint()
 {
@@ -39,7 +46,39 @@ async function getCookieFingerprint()
         cookieenable: navigator.cookieEnabled
     };
 }
+//Åtgärd 1. för att rensa cookies
+async function clearCookies()
+{
+    const cookies = document.cookie.split("; ");
+    for(let cookie of cookies)
+        {
+            const name=cookie.split("=")[0].trim();
+            document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+        }
+        alert("Cookies have been cleared.");
+        location.reload();
+}
+//Åtgärd 2. för att rensa cookie
+//Kommer att köras i bakgrunden per automatik.
+async function deleteCookie()
+{
+    setInterval(() => {
+        clearCookies();
+    }, 5000); // Rensa cookies var 5:e sekund
+}
 
+//Åtgärd 3. för att rensa cookie
+async function SessionsOnlyCookies()
+{
+    const cookies = document.cookie.split("; ");
+
+    cookies.forEach(cookie => {
+        const [name,value]=cookie.split("=").map(s=> s.trim());
+         document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
+        // Skapa ny utan expires (= session-only)
+        document.cookie = `${name}=${value};path=/;SameSite=Strict`;
+    });
+}
 
 
 
