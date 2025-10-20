60665a265ed6aa8131100e1c1ef95d47b7d6c2a5
ivbq <ivarbo@kth.se>
2025-10-20 21:53:07 +0200

    Refactor combination logic and add timezones to fingerprint

--- testing/test.py
+++ testing/test.py
@@ -1,34 +1,26 @@
 import os
 import json
+import itertools
 from playwright.sync_api import sync_playwright
-from itertools import combinations
 
 def main():
-    #sub_sets_of_perms = []
-    #for i in range(len(permissions) + 1):
-    #    t = combinations(permissions, i)
-    #    for item in t:
-    #        sub_sets_of_perms.append(list(item))
-
-    i = 0
     with open("output.json", "x") as f:
         with sync_playwright() as pw:
             browser = pw.chromium.launch()
-            for ua in user_agents:
-                i += 1
+            for i, ua, lc, tz, perms in enumerate(itertools.product(user_agents, locales, timezones, permissions)): 
                 print("Progress: " + str(round(i/len(user_agents), 3)))
-                for lc in locales:
-                    for perms in permissions:
-                        res = None
-                        try:
-                            page = browser.new_page(user_agent=ua, locale=lc, permissions=[perms])
-                            page.goto("file://" + abs_path)
-                            page.wait_for_selector("#fingerprint")
-                            res = page.locator("#fingerprint").text_content()
-                        except:
-                            pass
-                        finally: 
-                            f.write(json.dumps({"user_agent": ua, "locale" : lc, "permissions":perms, "hash": res})+ '\n')
+                res = None
+                try:
+                    page = browser.new_page(user_agent=ua, locale=lc, permissions=[perms])
+                    page.goto("file://" + abs_path)
+                    page.wait_for_selector("#fingerprint")
+                    res = page.locator("#fingerprint").text_content()
+                    cookie_info=page.locator("#cookies").text_content()
+                    print(cookie_info)
+                except:
+                    pass
+                finally: 
+                    f.write(json.dumps({"user_agent": ua, "locale" : lc, "timezone": tz, "permissions":perms, "hash": res})+ '\n')
             browser.close()
 
 abs_path = os.path.abspath("../index.html")
@@ -58,27 +50,6 @@ user_agents = [
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0"
 ]
 
-timezones = [
-    "America/New_York",
-    "America/Los_Angeles",
-    "America/Chicago",
-    "America/Sao_Paulo",
-    "America/Argentina/Buenos_Aires",
-    "Europe/London",
-    "Europe/Paris",
-    "Europe/Berlin",
-    "Europe/Stockholm",
-    "Europe/Moscow",
-    "Europe/Istanbul",
-    "Asia/Kolkata",
-    "Asia/Shanghai",
-    "Asia/Tokyo",
-    "Asia/Seoul",
-    "Australia/Sydney",
-    "Pacific/Auckland",
-    "Pacific/Honolulu"
-]
-
 locales = [
 "ar-SA",
 "bn-BD",
@@ -102,6 +73,27 @@ locales = [
 "nl-NL",
 "no-NO",
 "sv-SE"
+]
+
+timezones = [
+    "America/New_York",
+    "America/Los_Angeles",
+    "America/Chicago",
+    "America/Sao_Paulo",
+    "America/Argentina/Buenos_Aires",
+    "Europe/London",
+    "Europe/Paris",
+    "Europe/Berlin",
+    "Europe/Stockholm",
+    "Europe/Moscow",
+    "Europe/Istanbul",
+    "Asia/Kolkata",
+    "Asia/Shanghai",
+    "Asia/Tokyo",
+    "Asia/Seoul",
+    "Australia/Sydney",
+    "Pacific/Auckland",
+    "Pacific/Honolulu"
 ]
 
 permissions = [
